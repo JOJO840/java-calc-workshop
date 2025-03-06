653f4e54e29463009d04592acfff873cf16d2304
JOJO840 <josip840@gmail.com>
2025-03-06 10:10:44 +0100

    added the Calculator class for the logic

---
+++ src/main/java/org/lexicon/Calculator.java
@@ -0,0 +1,16 @@
+package org.lexicon;
+
+public class Calculator {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+    public static int sub(int a, int b) {
+        return a - b;
+    }
+    public static int mult(int a, int b) {
+        return a * b;
+    }
+    public static int div(int a, int b) {
+        return a / b;
+    }
+}
