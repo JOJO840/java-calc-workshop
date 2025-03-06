8aadeabfbbcec0d4f266de48c81e4a49a189c2e1
JOJO840 <josip840@gmail.com>
2025-03-06 10:57:18 +0100

    made logic for operator and addition also invalid input

--- src/main/java/org/lexicon/Main.java
+++ src/main/java/org/lexicon/Main.java
@@ -1,7 +1,28 @@
 package org.lexicon;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
-}
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter a operand: div:'/', multi:'*', div: '-', add:'+' or type 'q' to quit: ");
+            char operator = scanner.next().charAt(0);
+            if (operator == 'q' || operator == 'Q') {
+                break;
+            } if (operator != '+' && operator != '-' && operator != '*' && operator != '/') {
+                System.out.println("Invalid operator! Please enter one of +, -, *, /.");}
+            switch (operator) {
+                case '+':
+                    System.out.println("Enter the first number: ");
+                    int a = scanner.nextInt();
+                    System.out.println("Enter the second number: ");
+                    int b = scanner.nextInt();
+                    System.out.println("The result is: " + Calculator.add(a, b));
+                    break;
+            }
+
+
+        }// true
+    }//main method
+}//main class
